42ac1e6481b8c00a1d77acd617b62e1aaa8c8c66
abdou <abdou5246@gmail.com>
2026-07-25 18:10:42 +0200

    - refactor main + add select static function

--- main.c
+++ main.c
@@ -52,7 +52,8 @@ int read_input(InputBuffer *input_buffer) {
 
 typedef enum {
     COMMAND_SUCCESS,
-    UNRECOGNIZED_COMMAND
+    UNRECOGNIZED_COMMAND,
+    ID_DUPLICATE_ERROR
 } CommandResult;
 
 typedef struct {
@@ -76,6 +77,71 @@ CommandResult do_meta_command(InputBuffer *input_buffer) {
 }
 
 
+int insert_command(char **tokens, Table *table, int total_tokens) {
+    if (total_tokens > 4) {
+        printf("Too many arguments to insert!\n");
+        return COMMAND_SUCCESS;
+    }
+
+    if (table->num_rows == table->capacity) {
+        //reset errno
+        errno = 0;
+        Row *tmp = realloc(table->rows, table->capacity * 2 * sizeof(Row));
+        if (tmp == NULL) {
+            perror("Reallocation failed, try again\n");
+            return COMMAND_SUCCESS;
+        }
+        table->capacity = 2 * table->capacity;
+        table->rows = tmp;
+    }
+    errno = 0;
+    char *endptr;
+    long result = strtol(tokens[1], &endptr, 10);
+    if (errno != 0) {
+        perror("strtol");
+        return COMMAND_SUCCESS;
+    } else if (*endptr != '\0') {
+        printf("Invalid number: '%s'\n", endptr);
+        return COMMAND_SUCCESS;
+    } else if (result < INT_MIN || result > INT_MAX) {
+        printf("Value out of int range\n");
+        return COMMAND_SUCCESS;
+    } else {
+        table->rows[table->num_rows].id = result;
+        printf("Value = %ld\n", result);
+    }
+
+    // check if the id is not duplicate
+    for (int i =0; i<table->num_rows; i++) {
+        if (table->rows[i].id == result) {
+            printf("Id %ld is duplicated",result );
+            return ID_DUPLICATE_ERROR;
+        }
+    }
+    if (strlen(tokens[2]) >= sizeof(table->rows[table->num_rows].username)) {
+        printf("Username too long\n");
+        return COMMAND_SUCCESS;
+    }
+    strncpy(table->rows[table->num_rows].username, tokens[2],
+            sizeof(table->rows[table->num_rows].username) - 1);
+    table->rows[table->num_rows].username[sizeof(table->rows[table->num_rows].username) - 1] = '\0';
+    if (strlen(tokens[3]) >= sizeof(table->rows[table->num_rows].email)) {
+        printf("Email too long! \n");
+        return COMMAND_SUCCESS;
+    }
+    strncpy(table->rows[table->num_rows].email, tokens[3], sizeof(table->rows[table->num_rows].email));
+    table->rows[table->num_rows].email[sizeof(table->rows[table->num_rows].email) - 1] = '\0';
+    table->num_rows++;
+    return COMMAND_SUCCESS;
+}
+
+int select_all_command(Table *table) {
+    for (int i =0; i<table->num_rows; i++) {
+        printf("Row %d : %ld, %s, %s\n", i, table->rows[i].id, table->rows[i].username, table->rows[i].email);
+    }
+    return COMMAND_SUCCESS;
+}
+
 CommandResult process_command(InputBuffer *input_buffer, Table *table) {
     //split by whitespace first
     //variable to calculate the count of the tokens
@@ -83,7 +149,6 @@ CommandResult process_command(InputBuffer *input_buffer, Table *table) {
     int count = 0;
     int total_tokens = 0;
     const char *delim = " \t\n";
-    char *endptr;
     char *p = input_buffer->buffer;
     char *tokens[4];
 
@@ -105,61 +170,20 @@ CommandResult process_command(InputBuffer *input_buffer, Table *table) {
         return COMMAND_SUCCESS;
     }
 
+
+
     // Get first token
-    if (strcmp(tokens[0], "insert") != 0) {
+    if (strcmp(tokens[0], "insert") != 0 && strcmp(tokens[0], "select") != 0) {
         printf("Wrong command, try again!\n");
         return UNRECOGNIZED_COMMAND;
     }
 
-    if (total_tokens > 4) {
-        printf("Too many arguments to insert!\n");
-        return COMMAND_SUCCESS;
+    if (strcmp(tokens[0], "insert") == 0) {
+        return insert_command(tokens, table, total_tokens);
     }
-
-    if (table->num_rows == table->capacity) {
-        //reset errno
-        errno = 0;
-        Row *tmp = realloc(table->rows, table->capacity * 2 * sizeof(Row));
-        if (tmp == NULL) {
-            perror("Reallocation failed, try again\n");
-            return COMMAND_SUCCESS;
-        }
-        table->capacity = 2 * table->capacity;
-        table->rows = tmp;
+    else if (strcmp(tokens[0], "select") == 0) {
+        return select_all_command(table);
     }
-
-    //TODO : check if the ID is not duplicated
-    errno = 0;
-    long result = strtol(tokens[1], &endptr, 10);
-    if (errno != 0) {
-        perror("strtol");
-        return COMMAND_SUCCESS;
-    } else if (*endptr != '\0') {
-        printf("Invalid number: '%s'\n", endptr);
-        return COMMAND_SUCCESS;
-    } else if (result < INT_MIN || result > INT_MAX) {
-        printf("Value out of int range\n");
-        return COMMAND_SUCCESS;
-    } else {
-        table->rows[table->num_rows].id = result;
-        printf("Value = %ld\n", result);
-    }
-    if (strlen(tokens[2]) >= sizeof(table->rows[table->num_rows].username)) {
-        printf("Username too long\n");
-        return COMMAND_SUCCESS;
-    }
-    strncpy(table->rows[table->num_rows].username, tokens[2],
-            sizeof(table->rows[table->num_rows].username) - 1);
-    table->rows[table->num_rows].username[sizeof(table->rows[table->num_rows].username) - 1] = '\0';
-    if (strlen(tokens[3]) >= sizeof(table->rows[table->num_rows].email)) {
-        printf("Email too long! \n");
-        return COMMAND_SUCCESS;
-    }
-    strncpy(table->rows[table->num_rows].email, tokens[3], sizeof(table->rows[table->num_rows].email));
-    table->rows[table->num_rows].email[sizeof(table->rows[table->num_rows].email) - 1] = '\0';
-    table->num_rows++;
-
-    return COMMAND_SUCCESS;
 }
 
 int main(void) {
