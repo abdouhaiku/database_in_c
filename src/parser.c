3bb49cf02d0e90066a7603e9a168e8547b56cd5a
abdou <abdou5246@gmail.com>
2026-08-08 23:51:21 +0200

    skeleton functions for parsing expressions and statments

--- src/parser.c
+++ src/parser.c
@@ -5,6 +5,7 @@
 #include "parser.h"
 
 #include <stdlib.h>
+#include <string.h>
 
 void parser_init(Parser *parser, const char *sql) {
     Tokenizer *tokenizer = malloc(sizeof(Tokenizer));
@@ -17,7 +18,7 @@ void parser_advance(Parser *parser) {
     get_next_token(&parser->tokenizer, &parser->current);
 }
 
-AstNode *parse_expression(Parser *parser) {
+static AstNode *parse_primary_expression(Parser *parser) {
     AstNode *node = malloc(sizeof(AstNode));
 
     switch (parser->current.type) {
@@ -36,6 +37,8 @@ AstNode *parse_expression(Parser *parser) {
             node->as.column.length = parser->current.text_length;
             break;
         default:
+            // Not a literal or column reference - syntax error. Full error
+            // reporting is #41's job; for now, NULL signals "not an expression."
             free(node);
             return NULL;
     }
@@ -43,3 +46,72 @@ AstNode *parse_expression(Parser *parser) {
     return node;
 }
 
+AstNode *parse_expression(Parser *parser) {
+    AstNode *left = parse_primary_expression(parser);
+    if (left == NULL) {
+        return NULL;
+    }
+
+    // peek at whatever parse_primary_expression's advance left us sitting on.
+    if (parser->current.type != TOKEN_EQUAL) {
+        return left;
+    }
+    parser_advance(parser); // consume '='
+
+    AstNode *right = parse_primary_expression(parser);
+    if (right == NULL) {
+        ast_destroy(left);
+        return NULL;
+    }
+
+    AstNode *equals = malloc(sizeof(AstNode));
+    equals->type = EXPR_EQUALS;
+    equals->as.equals.left = left;
+    equals->as.equals.right = right;
+    return equals;
+}
+
+AstNode *parse_select_statement(Parser *parser) {
+    AstNode *node = malloc(sizeof(AstNode));
+    if (parser->current.type == TOKEN_SELECT) {
+        node->type = AST_SELECT;
+        parser_advance(parser);
+        // Get the column list
+        size_t column_count = 0;
+        AstNode *columns[3];
+        while (parser->current.type != TOKEN_EOF && parser->current.type != TOKEN_FROM) {
+            if (parser->current.type == TOKEN_COMMA) {
+                parser_advance(parser);
+            }
+            else if (parser->current.type == TOKEN_IDENTIFIER) {
+                columns[column_count] = parse_primary_expression(parser);
+                column_count++;
+            }
+            else {
+                //error while parsing
+                return NULL;
+            }
+        }
+
+        if (column_count > 0) {
+            for (size_t i = 0; i < column_count; i++) {
+                node->as.select.columns[i] = columns[i];
+            }
+        }
+        if (parser->current.type == TOKEN_FROM) {
+            parser_advance(parser);
+        }
+
+        // Get the name of the table
+        if (parser->current.type == TOKEN_IDENTIFIER) {
+            node->as.select.table = malloc(parser->current.text_length);
+            strncpy(node->as.select.table, parser->current.text, parser->current.text_length);
+            parser_advance(parser);
+        }
+
+        if (parser->current.type == TOKEN_WHERE) {
+            node->as.select.where = parse_expression(parser);
+        }
+    }
+
+}
